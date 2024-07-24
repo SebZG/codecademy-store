58ba2c94a13e7fb2953d644d02a64ccc557f2d37
Sebastian Zapata Gutierrez <sebastian.zapata.g@gmail.com>
2024-07-24 19:56:25 +0100

    Add starter files

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
-import App from './App';
+import App from './app/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const render = () => {
+	root.render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	);
+}
+
+render();
