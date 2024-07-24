5c6a6162e6ce55a40a65159d024c4a1672f21d29
Sebastian Zapata Gutierrez <sebastian.zapata.g@gmail.com>
2024-07-24 22:52:11 +0100

    Do import reducers @ store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,13 @@
 import { legacy_createStore as createStore, combineReducers } from "redux";
 
 import { inventoryReducer } from "../features/inventory/inventorySlice.js";
-// import { cartReducer } from "../features/cart/cartSlice.js";
-// import { currencyFilterReducer } from "../features/currencyFilter/CurrencyFilterSlice.js";
-// import { searchBarReducer } from "../features/searchBar/searchBarSlice.js";
+import { cartReducer } from "../features/cart/cartSlice.js";
+import { currencyFilterReducer } from "../features/currencyFilter/currencyFilterSlice.js";
+import { searchBarReducer } from "../features/searchBar/searchBarSlice.js";
 
 export const store = createStore(combineReducers({
    inventory: inventoryReducer,
-   // cart: cartReducer,
-   // currencyFilter: currencyFilterReducer,
-   // searchTerm: searchBarReducer
+   cart: cartReducer,
+   currencyFilter: currencyFilterReducer,
+   searchTerm: searchBarReducer
 }));
