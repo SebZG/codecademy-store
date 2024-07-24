7192e261bd8032f7d46f788b2d33ad12ddbdd425
Sebastian Zapata Gutierrez <sebastian.zapata.g@gmail.com>
2024-07-24 20:11:26 +0100

    Add Store

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
-import App from './app/App';
+import App from './app/App.js';
+
+import { store } from './app/store.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const render = () => {
 	root.render(
 		<React.StrictMode>
-			<App />
+			<App
+				state={store.getState()}
+				dispatch={store.dispatch}
+			/>
 		</React.StrictMode>
 	);
 }
 
 render();
+
+store.subscribe(render);
